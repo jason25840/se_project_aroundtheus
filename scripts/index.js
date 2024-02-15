4d4e0c051990c8077c746664f32aec9c2da2cc4e
J.Knight <jason25840@gmail.com>
2024-02-15 17:16:49 -0500

    added modal button function

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,15 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const profileCloseButton = document.querySelector("#profile-close-button");
+
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
