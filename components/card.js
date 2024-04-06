b5365c892756aa4fa3b2839bbe130185ecaa14be
J.Knight <jason25840@gmail.com>
2024-04-06 13:57:28 -0400

    refactor card

--- components/card.js
+++ components/card.js
@@ -1,45 +1,50 @@
 export default class Card {
-  constructor (data, cardSelector, handleImageClick) {
+  constructor(data, cardSelector, openPreviewModal) {
     this._name = data.name;
     this._link = data.link;
     this._cardSelector = cardSelector;
-    this._handleImageClick() {
-
-    }
+    this._openPreviewModal = openPreviewModal;
   }
 
-  setEventListeners() {
+  _setEventListeners() {
     //.card__like-button
-    this._cardElement.querySelector('.card__like-button')
-    .addEventListener('click', () => {
-      this._handleLikeIcon();
-    });
+    this._cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeIcon();
+      });
 
     //.card__trash-button
     this._cardElement
-    .querySelector('.card__trash-button')
-    .addEventListener('Click', () => {
-      this._handleTrashButton();
-     })
+      .querySelector(".card__trash-button")
+      .addEventListener("Click", () => {
+        this._handleTrashButton();
+      });
+
+    this._cardImageElement
+      .querySelector("#modal__preview-card")
+      .addEventListener("click", () => {
+        this._openPreviewModal(this);
+      });
   }
 
-_handleTrashButton() {
-  this._cardElement.remove();
-  this._cardElement = null;
-}
+  _handleTrashButton() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
 
-_handleLikeIcon() {
-  this._cardElement
-  .querySelector('.card__like-button')
-  .classList.toggle('card__like-button_active');
-}
+  _handleLikeIcon() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_active");
+  }
 
   getView() {
     this._cardElement = document
-    .querySelector(this._cardSelector)
-    .content.querySelector('.card')
-    .cloneNode(true);
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
 
-    this._setEeventListeners();
+    //this._setEventListeners();
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Card from "./card.js";
+import Card from "../components/card.js";
 
 const initialCards = [
   {
